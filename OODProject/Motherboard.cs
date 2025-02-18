97ba94098893639e802f59cc45e68b214a49abfb
Admin <Admin@DESKTOP-16VITVP>
2025-02-18 13:28:49 +0000

    Added motherboard class

--- OODProject/Motherboard.cs
+++ OODProject/Motherboard.cs
@@ -8,6 +8,16 @@ namespace OODProject
 {
     public class Motherboard : GenericPart
     {
-        public int MyProperty { get; set; }
+        public string Platform { get; set; }
+        public string Chipset { get; set; }
+        public int MaxMemoryCapacity { get; set; }
+        public int MemorySlots { get; set; }
+        public string FormFactor { get; set; }
+        public string MemoryType { get; set; }
+        public string PCIESlots { get; set; }
+        public string M2Slots { get; set; }
+        public string EthernetSupport { get; set; }
+        public string USBSupport { get; set; }
+        public string WIFISupport { get; set; }
     }
 }
