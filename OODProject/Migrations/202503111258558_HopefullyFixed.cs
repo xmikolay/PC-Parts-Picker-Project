875215f29b32d2a03fa3541ab092beb10b18cf48
Admin <Admin@DESKTOP-16VITVP>
2025-03-11 13:03:40 +0000

    Added CPUs data with correct identity seeding

---
+++ OODProject/Migrations/202503111258558_HopefullyFixed.cs
@@ -0,0 +1,16 @@
+﻿namespace OODProject.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class HopefullyFixed : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
