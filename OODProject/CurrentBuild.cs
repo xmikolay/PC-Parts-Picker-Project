c49d6e7c4a89754eb848a7164d0a7e7aafcaa68d
Admin <Admin@DESKTOP-16VITVP>
2025-04-07 14:46:18 +0100

    Created Class for current parts that are used in build to add filter by compatible parts feature

---
+++ OODProject/CurrentBuild.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Permissions;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OODProject
+{
+    public class CurrentBuild
+    {
+        public string CPUPlatform { get; set; }
+        public int CPUTdp { get; set; }
+        public int CoolerMaxTDP { get; set; }
+        public string MBPlatform { get; set; }
+        public string MBFormFactor { get; set; }
+        public int MBMaxMemoryCapacity { get; set; }
+        public string MBMemoryType { get; set; }
+        public int GPULength { get; set; }
+        public int GPUPsuReq { get; set; }
+        public int CaseMaxGPULength { get; set; }
+        public string CaseFormFactor { get; set; }
+        public int PSUPower { get; set; }
+        public int RAMCapacity { get; set; }
+        public string RAMType { get; set; } 
+    }
+}
